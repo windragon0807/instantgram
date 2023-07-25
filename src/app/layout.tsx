5437f6beb350591a2e1a72cc01849fc1c0a62b7e
ryong <tmdfyd95@naver.com>
2023-07-26 00:28:29 +0900

    ✨ layout, 헤더 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
+import Navbar from '@/components/Navbar';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -15,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={openSans.className}>
-      <body>{children}</body>
+      <body className='w-full max-w-screen-xl overflow-auto mx-auto'>
+        <header className='sticky top-0 bg-white z-10 border-b'>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import Link from 'next/link';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import HomeIcon from './ui/icons/HomeIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import { usePathname } from 'next/navigation';
+import ColorButton from './ui/ColorButton';
+
+const menu = [
+  {
+    href: '/',
+    icon: <HomeIcon />,
+    clickedIcon: <HomeFillIcon />,
+  },
+  {
+    href: '/search',
+    icon: <SearchIcon />,
+    clickedIcon: <SearchFillIcon />,
+  },
+  {
+    href: '/new',
+    icon: <NewIcon />,
+    clickedIcon: <NewFillIcon />,
+  },
+];
+
+export default function Navbar() {
+  const pathName = usePathname();
+  return (
+    <div className='flex justify-between items-center px-6'>
+      <Link href='/'>
+        <h1 className='text-3xl font-bold'>Instagram</h1>
+      </Link>
+      <nav>
+        <ul className='flex gap-4 items-center p-4'>
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+          <ColorButton text='Sign in' onClick={() => {}} />
+        </ul>
+      </nav>
+    </div>
+  );
+}
