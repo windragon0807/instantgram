6e08c3dc86df5771b3598e23f3cb92e97ae3aebc
ryong <tmdfyd95@naver.com>
2023-07-26 23:51:04 +0900

    ✨ 로그인 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import SearchFillIcon from './ui/icons/SearchFillIcon';
 import SearchIcon from './ui/icons/SearchIcon';
 import { usePathname } from 'next/navigation';
 import ColorButton from './ui/ColorButton';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const menu = [
   {
@@ -30,6 +31,8 @@ const menu = [
 
 export default function Navbar() {
   const pathName = usePathname();
+  const { data: session } = useSession();
+
   return (
     <div className='flex justify-between items-center px-6'>
       <Link href='/'>
@@ -44,7 +47,11 @@ export default function Navbar() {
               </Link>
             </li>
           ))}
-          <ColorButton text='Sign in' onClick={() => {}} />
+          {session ? (
+            <ColorButton text='Sign out' onClick={signOut} />
+          ) : (
+            <ColorButton text='Sign in' onClick={signIn} />
+          )}
         </ul>
       </nav>
     </div>
