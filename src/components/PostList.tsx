ebfad7f050aec3f6cd7cc2496a3b9e2597a2b128
ryong <tmdfyd95@naver.com>
2023-08-03 23:08:43 +0900

    ✨ 포스트 리스트 - 컴포넌트 구현

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,18 +1,29 @@
 "use client";
 
 import { SimplePost } from "@/model/post";
+import { GridLoader } from 'react-spinners';
 import useSWR from "swr";
+import PostListCard from './PostListCard';
 
 export default function PostList() {
   const { data: posts, isLoading: loading } = useSWR<SimplePost[]>("/api/posts");
   console.log(posts)
   return (
-    <ul>
-      {posts && posts.map(post => (
-        <li key={post.id}>
-            {post.text}
-        </li>
-      ))}
-    </ul>
+    <section>
+      {loading && (
+        <div>
+          <GridLoader color='red' />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
