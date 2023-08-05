221754a6f95e54acf3b18d55a68f4425dcc89abf
ryong <tmdfyd95@naver.com>
2023-08-05 23:46:58 +0900

    ✨ 포스트 리스트 - 리팩토링

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { SimplePost } from "@/model/post";
-import { GridLoader } from 'react-spinners';
 import useSWR from "swr";
 import PostListCard from './PostListCard';
+import GridSpinner from './ui/GridSpinner';
 
 export default function PostList() {
   const { data: posts, isLoading: loading } = useSWR<SimplePost[]>("/api/posts");
@@ -12,14 +12,14 @@ export default function PostList() {
     <section>
       {loading && (
         <div className='text-center mt-32'>
-          <GridLoader color='red' />
+          <GridSpinner color='red' />
         </div>
       )}
       {posts && (
         <ul>
-          {posts.map(post => (
+          {posts.map((post, index) => (
             <li key={post.id} className='mb-4'>
-              <PostListCard post={post} />
+              <PostListCard post={post} priority={index < 2} />
             </li>
           ))}
         </ul>
---
+++ src/components/ui/GridSpinner.tsx
@@ -0,0 +1,22 @@
+import dynamic from 'next/dynamic';
+
+const GridLoader = dynamic(
+    () => import('react-spinners').then((lib) => lib.GridLoader),
+    { ssr: false }
+);
+
+type Props = {
+  color?: string;
+};
+
+export default function GridSpinner({ color = 'red' }: Props) {
+  return <GridLoader color={color} />;
+}
+
+/**
+ * [Error]
+ * hydration-error-info.js:27 Warning: Prop `style` did not match.
+ * Server: "display:inline-block;background-color:red;width:15px;height:15px;margin:2px;border-radius:100%;animation-fill-mode:both;animation:react-spinners-GridLoader-grid 0.9496658407491304s 0.14966584074913042s infinite ease"
+ * Client: "display:inline-block;background-color:red;width:15px;height:15px;margin:2px;border-radius:100%;animation-fill-mode:both;animation:react-spinners-GridLoader-grid 0.6309172142807898s -0.16908278571921015s infinite ease"
+ * # Next.js는 서버에서 렌더링된 HTML을 클라이언트에서 재사용하기 때문에, 서버에서 렌더링된 HTML과 클라이언트에서 렌더링된 HTML이 일치하지 않으면 위와 같은 경고가 발생한다.
+ */
