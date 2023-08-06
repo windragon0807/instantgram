e7531c32554c1b1bf1c13fed183127a5ed58c27f
ryong <tmdfyd95@naver.com>
2023-08-06 23:52:22 +0900

    ✨ 사용자 포스트 - 스타일링 구현

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-type AvatarSize = 'small' | 'medium' | 'large';
+type AvatarSize = 'small' | 'medium' | 'large' | 'xlarge';
 type Props = {
   image?: string | null;
   size?: AvatarSize;
@@ -15,7 +15,7 @@ export default function Avatar({
       {/* 외부 url에 대한 포맷을 정하기 어려우므로 img 태그 사용 */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className={`bg-white object-cover rounded-full ${getImageSizeStyle(size)}`}
+        className={`bg-white object-cover rounded-full ${getImageSizeStyle(size).image}`}
         alt='user profile'
         src={image ?? undefined}
         // 외부 링크 사용해서 나타나는 x박스 이슈 해결
@@ -30,22 +30,38 @@ function getContainerStyle(size: AvatarSize, highlight: boolean): string {
   const highlightStyle = highlight
     ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
-  const sizeStyle = getContainerSize(size);
-  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+  const { container } = getImageSizeStyle(size);
+  return `${baseStyle} ${highlightStyle} ${container}`;
 }
 
-function getContainerSize(size: AvatarSize): string {
-  switch (size) {
-    case 'small': return 'w-10 h-10';
-    case 'medium': return 'w-12 h-12';
-    case 'large': return 'w-[65px] h-[65px]';
-  }
-}
+type ImageSizeStyle = {
+  container: string;
+  image: string;
+};
 
-function getImageSizeStyle(size: AvatarSize): string {
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
   switch (size) {
-    case 'small': return 'w-[34px] h-[34px] p-[0.1rem]';
-    case 'medium': return 'w-[42px] h-[42px] p-[0.1rem]';
-    case 'large': return 'w-16 h-16 p-[0.2rem]';
+    case 'small':
+      return {
+        container: 'w-10 h-10',
+        image: 'w-[34px] h-[34px] p-[0.1rem]'
+      };
+    case 'medium':
+      return {
+        container: 'w-12 h-12',
+        image: 'w-[42px] h-[42px] p-[0.1rem]'
+      };
+    case 'large':
+      return {
+        container: 'w-[65px] h-[65px]',
+        image: 'w-16 h-16 p-[0.2rem]'
+      };
+    case 'xlarge':
+      return {
+        container: 'w-[142px] h-[142px]',
+        image: 'w-[138px] h-[138px] p-[0.3rem]'
+      };
+    default:
+      throw new Error(`Unsupported type size: ${size}`);
   }
 }
