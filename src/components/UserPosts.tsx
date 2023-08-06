a1118a2f6f4e2979dafdace8334e97a53df278e9
ryong <tmdfyd95@naver.com>
2023-08-07 00:44:17 +0900

    ✨ 사용자 포스트 - 컴포넌트 구현

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -2,15 +2,35 @@
 
 import { ProfileUser } from '@/model/user';
 import { useState } from 'react';
-import useSWR from 'swr';
+import PostGrid from './PostGrid';
+import BookmarkIcon from './ui/icons/BookmarkIcon';
+import HeartIcon from './ui/icons/HeartIcon';
+import PostIcon from './ui/icons/PostIcon';
 
 type Props = {
   user: ProfileUser;
 };
 
+const tabs = [
+  { type: 'posts', icon: <PostIcon /> },
+  { type: 'saved', icon: <BookmarkIcon className='w-3 h-3' /> },
+  { type: 'posts', icon: <HeartIcon className='w-3 h-3' /> },
+];
+
 export default function UserPosts({ user: { username } }: Props) {
-  const [tab, setTab] = useState('posts');
-  const { data: posts, isLoading, error } = useSWR(`/api/users/${username}/${tab}`);
-  console.log(posts);
-  return <></>
+  const [query, setQuery] = useState(tabs[0].type);
+  
+  return (
+    <section>
+      <ul>
+        {tabs.map(({ type, icon }) => (
+          <li key={type} onClick={() => setQuery(type)}>
+            <button>{icon}</button>
+            <span>{type}</span>
+          </li>
+        ))}
+      </ul>
+      <PostGrid username={username} query={query} />
+    </section>
+  );
 }
--- src/components/ui/icons/BookmarkIcon.tsx
+++ src/components/ui/icons/BookmarkIcon.tsx
@@ -1,5 +1,9 @@
 import { RiBookmarkLine } from 'react-icons/ri';
 
-export default function BookmarkIcon() {
-  return <RiBookmarkLine className='w-6 h-6' />;
+type Props = {
+  className?: string;
+};
+
+export default function BookmarkIcon({ className }: Props) {
+  return <RiBookmarkLine className={className || 'w-6 h-6'} />;
 }
